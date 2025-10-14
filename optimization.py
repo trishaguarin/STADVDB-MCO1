3082f7fe63873cdec10a0d53f9cec66655fedc92
megandasal <megan_kei_dasal@dlsu.edu.ph>
2025-10-14 21:50:58 +0800

    Co-authored-by: trishaguarin <trishaguarin@users.noreply.github.com>

--- optimization.py
+++ optimization.py
@@ -10,7 +10,7 @@ engine = create_engine(
 def create_indexes():
 
     secondary_indexes = [
-        "CREATE INDEX indx_products_category ON DimProduucts(productID)",
+        "CREATE INDEX indx_products_category ON DimProducts(productID)",
         "CREATE INDEX indx_users_couriername ON DimRiders(couriername)",
         "CREATE INDEX indx_users_country ON DimUsers(country)",
         "CREATE INDEX indx_users_city ON DimUsers(city)",
@@ -33,7 +33,7 @@ def drop_indexes():
     """Drop all created indexes for testing purposes"""
     
     indexes_to_drop = [
-        "DROP INDEX indx_products_category ON DimProduucts",
+        "DROP INDEX indx_products_category ON DimProducts",
         "DROP INDEX indx_users_couriername ON DimRiders",
         "DROP INDEX indx_users_country ON DimUsers",
         "DROP INDEX indx_users_city ON DimUsers",
@@ -72,20 +72,23 @@ def analyze_performance():
     
     test_queries = {
         "Q1: Total Orders Over Time": 
-            """ SELECT DATE_FORMAT(o.createdAt, '%Y-%m') as period, # check
-                COUNT(DISTINCT o.orderID) as total_orders,
+            """ SELECT 
+                    DATE_FORMAT(o.createdAt, '%Y-%m') as period,
+                    COUNT(DISTINCT o.orderID) as total_orders
                 FROM FactOrders o
                 JOIN DimUsers u ON o.userID = u.userID
+                WHERE o.createdAt BETWEEN '2025-09-01' AND '2025-10-01'
+                GROUP BY DATE_FORMAT(o.createdAt, '%Y-%m')
             """,
 
         "Q2: Total Sales Per Location": 
             """ SELECT 
-                u.country as period,
-                SUM(o.quantity * p.price) as total_sales,
+                    u.country as location,
+                    SUM(o.quantity * p.price) as total_sales
                 FROM FactOrders o
                 JOIN DimUsers u ON o.userID = u.userID
                 JOIN DimProducts p ON o.productID = p.productID
-                WHERE o.createdAt BETWEEN '2025-09-01' AND '2025-10-01' AND u.country = 'Afghanistan'
+                WHERE o.createdAt BETWEEN '2025-09-01' AND '2025-10-01' AND u.country IN ('Afghanistan', 'Philippines', 'Samoa', 'United Kingdom')
                 GROUP BY u.country
                 ORDER BY total_sales DESC
             """,
@@ -93,32 +96,32 @@ def analyze_performance():
         "Q3: Total Sales per Product Category (per Time)": 
             """ SELECT 
                     p.category,
-                    SUM(o.quantity * p.price) as total_sales,
+                    SUM(o.quantity * p.price) as total_sales
                 FROM FactOrders o
                 JOIN DimProducts p ON o.productID = p.productID
                 JOIN DimUsers u ON o.userID = u.userID
-                WHERE o.createdAt BETWEEN '2025-09-01' AND '2025-10-01' AND u.country = 'Philippines'
+                WHERE o.createdAt BETWEEN '2025-09-01' AND '2025-10-01' AND u.country IN ('Afghanistan', 'Philippines', 'Samoa', 'United Kingdom')
                 GROUP BY p.category
                 ORDER BY total_sales DESC
-                LIMIT 100""",
+            """,
         
         "Q4: Orders by demographics": 
             """ SELECT 
-                u.gender,
-                CASE
-                    WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 18 AND 24 THEN '18-24'
-                    WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 25 AND 34 THEN '25-34'
-                    WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 35 AND 44 THEN '35-44'
-                    WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 45 AND 54 THEN '45-54'
-                    WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 55 AND 64 THEN '55-64'
-                    ELSE '65+'
-                END AS age_group,
-                u.country as location,
-                COUNT(DISTINCT o.orderID) as total_orders,
+                    u.gender,
+                    CASE
+                        WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 18 AND 24 THEN '18-24'
+                        WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 25 AND 34 THEN '25-34'
+                        WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 35 AND 44 THEN '35-44'
+                        WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 45 AND 54 THEN '45-54'
+                        WHEN TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 55 AND 64 THEN '55-64'
+                        ELSE '65+'
+                    END AS age_group,
+                    u.country as location,
+                    COUNT(DISTINCT o.orderID) as total_orders
                 FROM FactOrders o
                 JOIN DimUsers u ON o.userID = u.userID
                 JOIN DimProducts p ON o.productID = p.productID
-                WHERE o.createdAt BETWEEN '2025-09-01' AND '2025-10-01' AND u.country = 'Philippines' 
+                WHERE o.createdAt BETWEEN '2025-09-01' AND '2025-10-01' AND u.country IN ('Afghanistan', 'Philippines', 'Samoa', 'United Kingdom')
                         AND u.gender = 'F' AND TIMESTAMPDIFF(YEAR, u.dateofBirth, CURDATE()) BETWEEN 18 AND 24 
                 GROUP BY u.gender, age_group, u.country
                 ORDER BY total_orders DESC
